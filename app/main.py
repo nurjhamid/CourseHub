38576960e8a998123519b8f6516143dfb4c70a09
nurjhamid <nurjahan.hamid@northsouth.edu>
2025-12-03 19:13:54 +0600

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from app.controllers import api_router
+
+app = FastAPI(title="CourseHub")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # relaxed for prototype
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router)
+app.mount("/public", StaticFiles(directory="public"), name="public")
