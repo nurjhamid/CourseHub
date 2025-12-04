09a438afb19ccc9fccaf7bc5ee234dedfd2c2aa2
Md Fahim Khan <159687006+fryfahim@users.noreply.github.com>
2025-12-05 00:55:05 +0600

    Update conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,4 +1,3 @@
-# helper used by tests to create temporary in-memory DB engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models.database import Base
@@ -9,3 +8,4 @@ def create_in_memory_db():
     Base.metadata.create_all(bind=engine)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     return engine, SessionLocal
+
